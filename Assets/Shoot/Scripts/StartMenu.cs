1ed95cbee32fbbb9c2d7b0f400a8ba1f7ca7bea9
Pedro Rabinovitch <pedromr@gmail.com>
2016-08-13 22:20:16 -0400

    Added VR Prefs to start menu

--- Assets/Shoot/Scripts/StartMenu.cs
+++ Assets/Shoot/Scripts/StartMenu.cs
@@ -3,31 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour {
 
-	public void ToggleVRMode() {
-		Cardboard.SDK.VRModeEnabled = !Cardboard.SDK.VRModeEnabled;
-	}
-
 	public void StartGame() {
+		VRPreferences.Instance.SaveSettings();
 		SceneManager.LoadScene("Game");
 	}
 
 
-	public void ToggleDistortionCorrection() {
-		switch(Cardboard.SDK.DistortionCorrection) {
-			case Cardboard.DistortionCorrectionMethod.Unity:
-				Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.Native;
-				break;
-			case Cardboard.DistortionCorrectionMethod.Native:
-				Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.None;
-				break;
-			case Cardboard.DistortionCorrectionMethod.None:
-			default:
-				Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.Unity;
-				break;
-		}
-	}
-
-	public void ToggleDirectRender() {
-		Cardboard.Controller.directRender = !Cardboard.Controller.directRender;
-	}
 }
