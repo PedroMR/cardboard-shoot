b04ec2ea5ac8f0274eb2df3d91ad83c4f51583aa
Pedro Rabinovitch <pedromr@gmail.com>
2016-08-15 20:26:59 -0400

    Improving missile a bit

--- Assets/Shoot/Scripts/CityTarget.cs
+++ Assets/Shoot/Scripts/CityTarget.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 [RequireComponent(typeof(WeaponTargetable))]
 public class CityTarget : MonoBehaviour
@@ -21,5 +22,15 @@ public class CityTarget : MonoBehaviour
 	void Awake()
 	{
 		weaponTarget = GetComponent<WeaponTargetable>();
+		weaponTarget.SufferedLethalDamage += OnSufferedLethalDamage;
+	}
+
+	public void OnSufferedLethalDamage(WeaponTargetable which)
+	{
+		transform.parent.DOMoveY(-15, 5);
+
+		var original = transform.parent.position;
+		transform.parent.DOShakePosition(10, (Vector3.right + Vector3.forward) * 0.3f);
+//		transform.parent.DOMoveX(original., 5);
 	}
 }
