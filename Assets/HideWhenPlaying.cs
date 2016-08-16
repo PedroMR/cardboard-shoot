fca2151e00b2405d3742c83045d37cfe85cb5c59
Pedro Rabinovitch <pedromr@gmail.com>
2016-08-16 14:39:29 -0400

    New targeting reticles

---
+++ Assets/HideWhenPlaying.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideWhenPlaying : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		gameObject.SetActive(false);
+	}
+}
