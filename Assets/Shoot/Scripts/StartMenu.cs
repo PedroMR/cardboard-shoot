efaa91324a5c2c75dafe5359ebb0391589d96bb7
Pedro Rabinovitch <pedromr@gmail.com>
2016-08-17 12:12:32 -0400

    Cloning start menu to make it easier to find

--- Assets/Shoot/Scripts/StartMenu.cs
+++ Assets/Shoot/Scripts/StartMenu.cs
@@ -2,9 +2,28 @@
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour {
+	public GameObject uiMenu;
 
 	void Start() {
 		Cardboard.SDK.Recenter();
+
+		if (uiMenu != null) {
+			var menuCopy = (GameObject)GameObject.Instantiate(uiMenu, uiMenu.transform.position, uiMenu.transform.rotation);
+
+			MirrorPosition(menuCopy);
+		}
+	}
+
+	void MirrorPosition(GameObject menu) {
+		var newPos = menu.transform.position;
+
+		newPos.x = -newPos.x;
+		newPos.z = -newPos.z;
+
+		menu.transform.position = newPos;
+
+		menu.transform.Rotate(0, 180, 0);
+			
 	}
 
 	public void StartGame() {
