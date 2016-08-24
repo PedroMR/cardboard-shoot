86c290dd5702f4811f3be0074b097306beef9d02
Pedro Rabinovitch <pedromr@gmail.com>
2016-08-24 17:50:28 -0400

    Sketching level config

---
+++ Assets/Shoot/Scripts/LevelConfig.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelConfig : ScriptableObject
+{
+	public class WaveConfig : ScriptableObject {
+
+		FloatRange	TimeBetweenWaves;
+		float		SpawnAngleSpread;
+		FloatRange	SpawnElevation;
+
+		EnemyCandidates[]	Enemies;
+
+	}
+
+	public class EnemyCandidates : ScriptableObject {
+		EnemyGroup	EnemyGroup;
+		float		Weight;
+	}
+
+	public class EnemyGroup : ScriptableObject {
+		GameObject	EnemyPrefab;
+		FloatRange	Count;
+		FloatRange	SpawnDistance;
+	}
+
+	[SerializeField]
+	public WaveConfig[] waveConfig;
+}
+
