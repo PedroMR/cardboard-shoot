72c81bc89a946f54a742a0576c151599f93bb738
Pedro Rabinovitch <pedromr@gmail.com>
2016-10-07 17:15:16 -0400

    Skygrid start and some android compilation stuff

---
+++ Assets/Shoot/Scripts/SkyGrid.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyGrid : MonoBehaviour
+{
+	void Awake()
+	{
+		var sphere = GetComponent<MeshFilter>();
+
+		var mesh = sphere.mesh;
+
+		var normals = mesh.normals;
+
+		for(var i=0; i < normals.Length; i++) {
+			var n = normals[i];
+			normals[i] = -n;
+		}
+		mesh.normals = normals;
+	}
+
+}
+
